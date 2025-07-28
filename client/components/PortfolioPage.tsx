159518340e22d61ca81be77679dc2fc994d73a5a
Pralhad Neupane <155094146+pralhad-69@users.noreply.github.com>
2025-07-28 09:50:35 +0545

    Update PortfolioPage.tsx

--- client/components/PortfolioPage.tsx
+++ client/components/PortfolioPage.tsx
@@ -3,7 +3,6 @@ import ScrollProgress from "./ScrollProgress";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Skills from "./sections/Skills";
-import Projects from "./sections/Projects";
 import Services from "./sections/Services";
 import Contact from "./sections/Contact";
 import Footer from "./Footer";
@@ -30,7 +29,6 @@ const PortfolioPage = () => {
         <Hero />
         <About />
         <Skills />
-        <Projects />
         <Services />
         <Contact />
       </main>
